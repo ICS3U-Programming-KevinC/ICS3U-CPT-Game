8a7e020a0207f0738e8a522a05114cd79330eca1
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2023-01-11 17:03:28 -0500

    Add files via upload

--- code.py
+++ code.py
@@ -16,11 +16,13 @@ def game_scene():
     # sets the size of the background grid to 10*8
     background = stage.Grid(image_bank_background, 10, 8)
 
+    # sets the position of the 4 player sprites
     player1 = stage.Sprite(image_bank_sprites, 2, 67, 66)
     player2 = stage.Sprite(image_bank_sprites, 3, 67, 82)
     player3 = stage.Sprite(image_bank_sprites, 4, 83, 66)
     player4 = stage.Sprite(image_bank_sprites, 5, 83, 82)
 
+    # sets player to all of the player sprites
     player = [player1] + [player2] + [player3] + [player4]
 
     # displays the background and sets the frame rate to 60
@@ -33,7 +35,31 @@ def game_scene():
     game.render_block()
 
     while True:
-        pass
+        # handle inputs
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("START")
+        if keys & ugame.K_SELECT:
+            print("SELECT")
+        if keys & ugame.K_RIGHT:
+            player.move(player.x + 1, player.y)
+        if keys & ugame.K_LEFT:
+            player.move(player.x - 1, player.y)
+        if keys & ugame.K_UP:
+            player.move(player.x, player.y - 1)
+        if keys & ugame.K_DOWN:
+            player.move(player.x, player.y + 1)      
+
+        # renders the sprite every frame
+        game.render_sprites([player])
+
+        # only ticks every 1/60th of a second
+        game.tick()
 
 
 if __name__ == "__main__":
